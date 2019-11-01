d88b55cd87e2f99faab19995acf3ea8680aa1892
vakarevds <vakarevds@yandex-team.ru>
2019-11-02 00:32:51 +0700

    Refactor test App renders without crashing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App: React.FC = () => {
     return (
-        <div className="App">
+        <div className="app">
             <header className="App-header">Hello world!</header>
         </div>
     );
--- app/src/__tests__/App.test.tsx
+++ app/src/__tests__/App.test.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
 import App from '../App';
 
 describe('App', () => {
+    let container: HTMLDivElement | null;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        if (container) {
+            document.body.removeChild(container);
+            container = null;
+        }
+    });
+
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        act(() => {
+            ReactDOM.render(<App />, container);
+        });
+
+        const app = container && container.getElementsByClassName('app')[0];
+
+        expect(Boolean(app)).toBe(true);
     });
 });
